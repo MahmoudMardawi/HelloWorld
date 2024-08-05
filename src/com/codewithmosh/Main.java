bad8deef7e973f684dd89f41baa0c10047835564
MahmoudMardawi <ma.mardawi@gmail.com>
2024-08-05 15:43:51 +0300

    assigning variables

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -3,6 +3,9 @@ package com.codewithmosh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int myAge = 30;
+        int herAge = myAge;
+        age = 35;
+        System.out.println(herAge);
     }
 }
