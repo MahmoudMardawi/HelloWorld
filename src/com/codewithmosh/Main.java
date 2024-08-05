76926ffa92ae2c79522edc2060f811853c746a4a
MahmoudMardawi <ma.mardawi@gmail.com>
2024-08-05 16:14:18 +0300

    write reference type example

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,13 +1,12 @@
 package com.codewithmosh;
 
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        byte myAge = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-        System.out.println();
+        byte age = 30;
+        Date now = new Date();
+        System.out.println("now: "+now);
     }
 }
